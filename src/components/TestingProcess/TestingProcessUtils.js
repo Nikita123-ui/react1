a720abf9d8761e8d8eb7080be434973c5f20b25c
Nikita <nikitavaryani2015@gmail.com>
2022-06-21 16:17:40 +0530

    changes add

--- src/components/TestingProcess/TestingProcessUtils.js
+++ src/components/TestingProcess/TestingProcessUtils.js
@@ -1,38 +1,44 @@
 export const testingProcessType = [
-  {
-    tpText: '1',
-    headText: 'Initiation',
-    pText:
-      "With Galaxy, we'll define the scope of work, specific tasks, and discuss deliverables and timelines.",
-  },
-  {
-    tpText: '2',
-    headText: 'Requirement Gathering',
-    pText:
-      'Tell us your expectations, goals, and what you want the project to accomplish.',
-  },
-  {
-    tpText: '3',
-    headText: 'Test Requirements & Planning',
-    pText:
-      "You'll receive a custom-designed test plan with specific objectives, resources, and processes. We'll assess risk to identify potential threats and possible hurdles.",
-  },
-  {
-    tpText: '4',
-    headText: 'Environment Setup',
-    pText:
-      "We'll decide the right software and hardware configuration for you in order to execute the software that will allow us to examine all the application functionalities.",
-  },
-  {
-    tpText: '5',
-    headText: 'Execution and Defect Tracking',
-    pText:
-      'Extensive testing will allow us to record all the statistics found in the results, review it for all the modules, track the defects found, and generate insights for you.',
-  },
-  {
-    tpText: '6',
-    headText: 'Iterate',
-    pText:
-      "In case there are areas that need worked on again, we'll take care of it. We neither want our partners or our software to be bugged because you deserve a fully optimized site/app.",
-  },
+  [
+    {
+      tpText: '1',
+      headText: 'Initiation',
+      pText:
+        "With Galaxy, we'll define the scope of work, specific tasks, and discuss deliverables and timelines.",
+    },
+    {
+      tpText: '2',
+      headText: 'Requirement Gathering',
+      pText:
+        'Tell us your expectations, goals, and what you want the project to accomplish.',
+    },
+  ],
+  [
+    {
+      tpText: '3',
+      headText: 'Test Requirements & Planning',
+      pText:
+        "You'll receive a custom-designed test plan with specific objectives, resources, and processes. We'll assess risk to identify potential threats and possible hurdles.",
+    },
+    {
+      tpText: '4',
+      headText: 'Environment Setup',
+      pText:
+        "We'll decide the right software and hardware configuration for you in order to execute the software that will allow us to examine all the application functionalities.",
+    },
+  ],
+  [
+    {
+      tpText: '5',
+      headText: 'Execution and Defect Tracking',
+      pText:
+        'Extensive testing will allow us to record all the statistics found in the results, review it for all the modules, track the defects found, and generate insights for you.',
+    },
+    {
+      tpText: '6',
+      headText: 'Iterate',
+      pText:
+        "In case there are areas that need worked on again, we'll take care of it. We neither want our partners or our software to be bugged because you deserve a fully optimized site/app.",
+    },
+  ],
 ];
--- src/components/TestingProvide/TestingProvideUtils.js
+++ src/components/TestingProvide/TestingProvideUtils.js
@@ -1,50 +1,58 @@
 export const testingType = [
-  {
-    numberText: '1',
-    titleText: 'Functional Testing',
-    paraText:
-      "We'll generate an extensive list of functional test cases and scenarios to be executed across various Windows and Mac devices",
-  },
-  {
-    numberText: '2',
-    titleText: 'Automation Testing',
-    paraText:
-      'Would you like a robust test automation setup that your team can run before releasing website updates?',
-  },
-  {
-    numberText: '3',
-    titleText: 'API Testing',
-    paraText:
-      "We will ensure that your API's run coherently and avoid any corner cases, delay in processing, or security breaches for internal or external API.",
-  },
-  {
-    numberText: '4',
-    titleText: 'UX Testing',
-    paraText:
-      ' Harnessing a capable team of in-house UX experts that will acquire feedback from end users that are similar to target audience to inform our UX report.',
-  },
-  {
-    numberText: '5',
-    titleText: 'Cross-Browser Testing',
-    paraText:
-      'Galaxy uses a mixture of manual and automated techniques to deliver an insightful report that covers cross-browser compatibility checks for your website or app.',
-  },
-  {
-    numberText: '6',
-    titleText: 'Performance Testing',
-    paraText:
-      'Does your website crash or run slow when too many people simultaneously interact with your site? A slow or crashing site could mean losing a potential company.',
-  },
-  {
-    numberText: '7',
-    titleText: 'Security Testing',
-    paraText:
-      "Security issues not only impact your users, but also your company's reputation. Galaxy can help you identify broad-level security issues with your website and prevent these damages.",
-  },
-  {
-    numberText: '8',
-    titleText: 'Payment Testing',
-    paraText:
-      "We perform global payment testing for your e-commerce sites, in-app purchases, POS, and mobile wallets. This will improve your customer's experience, build trust, that there's complete flexibility.",
-  },
+  [
+    {
+      numberText: '1',
+      titleText: 'Functional Testing',
+      paraText:
+        "We'll generate an extensive list of functional test cases and scenarios to be executed across various Windows and Mac devices",
+    },
+    {
+      numberText: '2',
+      titleText: 'Automation Testing',
+      paraText:
+        'Would you like a robust test automation setup that your team can run before releasing website updates?',
+    },
+  ],
+  [
+    {
+      numberText: '3',
+      titleText: 'API Testing',
+      paraText:
+        "We will ensure that your API's run coherently and avoid any corner cases, delay in processing, or security breaches for internal or external API.",
+    },
+    {
+      numberText: '4',
+      titleText: 'UX Testing',
+      paraText:
+        ' Harnessing a capable team of in-house UX experts that will acquire feedback from end users that are similar to target audience to inform our UX report.',
+    },
+  ],
+  [
+    {
+      numberText: '5',
+      titleText: 'Cross-Browser Testing',
+      paraText:
+        'Galaxy uses a mixture of manual and automated techniques to deliver an insightful report that covers cross-browser compatibility checks for your website or app.',
+    },
+    {
+      numberText: '6',
+      titleText: 'Performance Testing',
+      paraText:
+        'Does your website crash or run slow when too many people simultaneously interact with your site? A slow or crashing site could mean losing a potential company.',
+    },
+  ],
+  [
+    {
+      numberText: '7',
+      titleText: 'Security Testing',
+      paraText:
+        "Security issues not only impact your users, but also your company's reputation. Galaxy can help you identify broad-level security issues with your website and prevent these damages.",
+    },
+    {
+      numberText: '8',
+      titleText: 'Payment Testing',
+      paraText:
+        "We perform global payment testing for your e-commerce sites, in-app purchases, POS, and mobile wallets. This will improve your customer's experience, build trust, that there's complete flexibility.",
+    },
+  ],
 ];
